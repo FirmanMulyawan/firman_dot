4f4ac9fac92d1e42c7eb19225d5482cec7bc181b
FirmanMulyawan <firmanmulyawan491@gmail.com>
2025-09-11 19:37:12 +0700

    add home page

--- lib/component/config/app_style.dart
+++ lib/component/config/app_style.dart
@@ -2,83 +2,52 @@ import 'package:flutter/material.dart';
 
 class AppStyle {
   static Color textColor = const Color(0xff333333);
+  static Color grey1 = const Color(0xff4F4F4F);
   static Color grey3 = const Color(0xff828282);
   static Color blue = const Color(0xff0A97B0);
+  static Color blue1 = const Color(0xff2F80ED);
+  static Color blue2 = const Color(0xff2D9CDB);
+  static Color blue3 = const Color(0xff56CCF2);
   static Color whiteColor = Colors.white;
   static Color teal = const Color(0xff46B5A7);
+  static Color yellow = const Color(0xffF2C94C);
+  static Color orange = const Color(0xffF2994A);
+  static Color red = const Color(0xffEB5757);
+  static Color purple1 = const Color(0xff9B51E0);
+  static Color purple2 = const Color(0xffBB6BD9);
+  static Color green2 = const Color(0xff27AE60);
 
 //
-  static Color textFieldColor = const Color(0xff40807E);
-  static Color textFieldBgColor = const Color(0xffDDE7E4);
   static Color hintColor = const Color(0xffD7E3DF);
   static Color errorTextColor = const Color(0xffFF5252);
   static Color borderColor = const Color(0xff669585);
   static Color buttonDisabledColor = const Color(0xffCCDBD6);
   static Color buttonDisabledShadowColor = const Color(0xff99A3A0);
 
-  static Color borderGreen = const Color(0xffB9D8A0);
   static Color mainGreen = const Color(0xff90BE6D);
   static Color hoverGreen = const Color(0xff43AA8B);
-  static Color softGreen = const Color(0xffA5CC86);
   static Color green = const Color(0xff418E83);
   static Color pressedGreen = const Color(0xff40807E);
-  static Color learnGreen = const Color(0xff6fdc8e);
-  static Color profleGreen = const Color(0xff3E997E);
-  static Color ashyGreen = const Color(0xfff3f6f5);
-  static Color lightMintGreen = const Color(0xffddf5cb);
-  static Color dustyGreen = const Color(0xff8ab864);
 
   static Color borderBlue = const Color(0xff81DEF0);
   static Color mainBlue = const Color(0xff25C7E8);
   static Color hoverBlue = const Color(0xff17AEC4);
   static Color pressedBlue = const Color(0xff2C8DB0);
 
-  static Color borderYellow = const Color(0xffFED380);
-  static Color mainYellow = const Color(0xffF9C74F);
   static Color hoverYellow = const Color(0xffEDAD14);
   static Color pressedYellow = const Color(0xffC38C06);
   static Color yellowBackground = const Color(0xfffcd51b);
   static Color yellowShadow = const Color(0xffffbb25);
 
-  static Color orange700 = const Color(0xffFB8702);
-  static Color orange300 = const Color(0xffFEA032);
-  static Color orange100 = const Color(0xffFFA338);
-  static Color borderOrange = const Color(0xffFFB978);
-  static Color mainOrange = const Color(0xffF8961E);
   static Color hoverOrange = const Color(0xffE27B02);
   static Color pressedOrange = const Color(0xffF66A03);
 
-  static Color otherRed = const Color(0xffFFBFBF);
-  static Color borderRed = const Color(0xffF98A8A);
   static Color mainRed = const Color(0xffFF5252);
   static Color hoverRed = const Color(0xffE31A37);
   static Color pressedRed = const Color(0xffA40219);
-  static Color redsecondary = const Color(0xffDD0800);
-  static Color paleRose = const Color(0xffffdfdf);
-  static Color softCoralPink = const Color(0xfffec8c5);
-
-  static Color grey200 = const Color(0xffe8eeec);
-  static Color grey300 = const Color(0xff767676);
-  static Color grey600 = const Color(0xffBAC7C3);
-  static Color grey777 = const Color(0xff777777);
-  static Color grey40 = const Color(0xff404040);
-  static Color greyLine = const Color(0xff464646);
-  static Color grey5 = const Color(0xffE0E0E0);
-  static Color grey500 = const Color(0xff577590);
-  static Color grey900 = const Color(0xffDFE9E6);
-  static const Color lightGrayishCyan = Color(0xFFBAC7C3);
-
-  static Color buttonOrange = const Color(0xffFA8701);
-  static Color flagGray = const Color(0xffEFF4F2);
-
-  static Color hoverWhite = const Color(0xffe6ecea);
-  static Color darkGreen = const Color(0xff014F34);
-  static Color progressBlue = const Color(0xff27B9ED);
-  static Color electricGlow = const Color(0xfffdd100);
 
   static Color progressBarTopBlue = const Color(0xff8EE3F4);
   static Color progressBarBottomBlue = const Color(0xff1DC7E9);
-  static Color circleBorderYellow = const Color(0xffEDB000);
 
   static Color homeLearnGreen = const Color(0xff62AA8F);
   static Color homeLearnHover = const Color(0xff507E7D);
@@ -86,55 +55,16 @@ class AppStyle {
   static Color homeAssessmentOrange = const Color(0xffEB8D34);
   static Color homeAssessmentHover = const Color(0xffE5732E);
 
-  static Color homeLeaderboardHover = const Color(0xffD79B09);
-
   static Color homeArabicBlue = const Color(0xff3EBDEA);
   static Color homeArabicHover = const Color(0xff359CC1);
 
   static Color homePrayerGreen = const Color(0xff4FD371);
   static Color homePrayerHover = const Color(0xff3B9E55);
-  static Color homePrayerCircle = const Color(0xff43B761);
 
   static Color homeYoutubeRed = const Color(0xffFD4D43);
   static Color homeYoutubeHover = const Color(0xffBE3A32);
   static Color homeYoutubeButton = const Color(0xffF3CB2E);
   static Color homeYoutubeShadow = const Color(0xffF68400);
-
-  static Color backgroundXpColor = const Color(0x1a000000);
-
-  static Color searchBorderColor = const Color(0xffD9E5E5);
-  static Color searchHintColor = const Color(0xffE0E0E0);
-  static Color savedPrayerGreen = const Color(0xff6CC09D);
-
-  static Color detailPrayerBg = const Color(0xffF3F6F5);
-  static Color planGreen = const Color(0xff407F7E);
-  static Color grey4 = const Color(0xffBDBDBD);
-  static Color planGrey = const Color(0xffA5B5B0);
-
-  static Color softPurple = const Color(0xffC249EC);
-  static Color darkPurple = const Color(0xff7D49EC);
-  static Color softPurple100 = const Color(0xffA483EC);
-
-  static Color leaderboardYellow1 = const Color(0xffF3CE38);
-  static Color leaderboardYellow2 = const Color(0xffDEB40B);
-  static Color leaderboardYellow3 = const Color(0xffE1B400);
-  static Color yellow600 = const Color(0xffE8BA00);
-  static Color leaderboardProfileYellow = const Color(0xffFFD821);
-
-  static Color rank1Border = const Color(0xffC19B00);
-  static Color rank1Color = const Color(0xffEEC624);
-  static Color rank1Text = const Color(0xffffffff);
-  static Color rank1Point = const Color(0xffE0A412);
-
-  static Color rank2Border = const Color(0xffBFBFBF);
-  static Color rank2Color = const Color(0xffE6ECEA);
-  static Color rank2Text = const Color(0xff707876);
-  static Color rank2Point = const Color(0xff909C98);
-
-  static Color rank3Border = const Color(0xffD09255);
-  static Color rank3Color = const Color(0xffDBA672);
-  static Color rank3Text = const Color(0xffffffff);
-  static Color rank3Point = const Color(0xffBC8550);
 
   static Color dialogBgColor = const Color(0xff014F34).withValues(alpha: 0.4);
 
--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
+import '../../../component/config/app_const.dart';
 import '../../../component/config/app_style.dart';
 import 'home_controller.dart';
+import 'widget/expense_category_card.dart';
 
 class HomeScreen extends GetView<HomeController> {
   const HomeScreen({super.key});
@@ -10,52 +13,119 @@ class HomeScreen extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: AppStyle.whiteColor,
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Halo, User!",
-                  style: AppStyle.bold(size: 18),
-                ),
-                SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  "Jangan lupa catat keuanganmu setiap hari!",
-                  style: AppStyle.regular(textColor: AppStyle.grey3),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    Expanded(
-                      child: _cardExpense(
-                          title: 'Pengeluaranmu\nhari ini', nominal: "30.000"),
-                    ),
-                    SizedBox(
-                      width: 19,
-                    ),
-                    Expanded(
-                      child: _cardExpense(
-                          color: AppStyle.teal,
-                          title: 'Pengeluaranmu\nbulan ini',
-                          nominal: "335.500"),
-                    )
-                  ],
-                )
-              ],
+      backgroundColor: AppStyle.whiteColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Halo, User!",
+                style: AppStyle.bold(size: 18),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Text(
+                "Jangan lupa catat keuanganmu setiap hari!",
+                style: AppStyle.regular(textColor: AppStyle.grey3),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: _cardExpense(
+                        title: 'Pengeluaranmu\nhari ini', nominal: "30.000"),
+                  ),
+                  SizedBox(
+                    width: 19,
+                  ),
+                  Expanded(
+                    child: _cardExpense(
+                        color: AppStyle.teal,
+                        title: 'Pengeluaranmu\nbulan ini',
+                        nominal: "335.500"),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Pengeluaran berdasarkan kategori",
+                style: AppStyle.bold(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ExpenseCategoryCard(
+                backgroundColorIcon: AppStyle.yellow,
+                iconCard: AppConst.iconBasketball,
+                title: "Internet",
+                totalAmount: "Rp. 20.000",
+              ),
+              SizedBox(
+                height: 28,
+              ),
+              Text(
+                "Hari ini",
+                style: AppStyle.bold(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              _dailyExpenseCards(
+                  colorIcon: AppStyle.blue,
+                  iconName: AppConst.iconBasketball,
+                  title: 'Ayam Geprek',
+                  price: 'Rp. 15.000'),
+              SizedBox(
+                height: 28,
+              ),
+              Text(
+                "Kemarin",
+                style: AppStyle.bold(),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              _dailyExpenseCards(
+                  colorIcon: AppStyle.blue,
+                  iconName: AppConst.iconBasketball,
+                  title: 'Ayam Geprek',
+                  price: 'Rp. 15.000'),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        child: Container(
+          width: 56,
+          height: 56,
+          decoration: BoxDecoration(
+            color: AppStyle.blue,
+            shape: BoxShape.circle,
+          ),
+          child: Center(
+            child: SvgPicture.asset(
+              AppConst.iconUilPlus,
+              width: 24,
+              height: 24,
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 
   Widget _cardExpense({Color? color, String title = '', String nominal = ''}) {
@@ -78,6 +148,57 @@ class HomeScreen extends GetView<HomeController> {
           ),
           Text("Rp. $nominal",
               style: AppStyle.bold(textColor: AppStyle.whiteColor, size: 18))
+        ],
+      ),
+    );
+  }
+
+  Widget _dailyExpenseCards(
+      {String? iconName,
+      Color? colorIcon,
+      String title = '',
+      String price = ''}) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(horizontal: 14, vertical: 22),
+      decoration: BoxDecoration(
+        color: AppStyle.whiteColor,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.2),
+            blurRadius: 10,
+            spreadRadius: 1,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Center(
+            child: SvgPicture.asset(
+              iconName ?? '',
+              width: 24,
+              height: 24,
+              colorFilter: ColorFilter.mode(
+                colorIcon ?? AppStyle.yellow,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 14,
+          ),
+          Expanded(
+            child: Text(
+              title,
+              style: AppStyle.regular(),
+            ),
+          ),
+          Text(
+            price,
+            style: AppStyle.medium(),
+          ),
         ],
       ),
     );
